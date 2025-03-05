27323c0e808f4a3e83b93f5f49b829653df0bae9
kung-fu-mari <ifcodes@duck.com>
2025-03-05 04:23:40 -0700

    Initial commit

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,27 +1,22 @@
 extends CharacterBody2D
 
-@onready var anim_player = $AnimationPlayer
+@onready var anim_player := $AnimationPlayer
+@onready var hitbox := $SpriteContainer/RightHandSprite/SwordSprite/Hitbox
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED := 300.0
+const JUMP_VELOCITY := -400.0
 
-# these two variables help stop 
-# the animation player from restarting the run anim every physics frame
-# which causes mr bean to only do the first half of run anim
-var running = false
-var already_running = false
+var running := false
+var swinging := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		running = true
@@ -31,12 +26,33 @@ func _physics_process(delta: float) -> void:
 			$SpriteContainer.scale.x = 1
 		velocity.x = direction * SPEED
 	else:
-		anim_player.pause()
-		running = false
-		already_running = false
+		if velocity.x == 0:
+			running = false
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if running == true and already_running == false:
-		already_running = true
-		anim_player.play('run')
 	move_and_slide()
+	
+func _process(_delta: float) -> void:
+	if global_position.y > 550:
+		get_tree().reload_current_scene()
+	check_for_attack()
+	handle_animation()
+
+func check_for_attack() -> void:
+	if Input.is_action_pressed("attack") and swinging == false:
+		swinging = true
+		hitbox.set_collision_layer_value(1, true)
+
+func handle_animation() -> void:
+	if swinging:
+		if anim_player.current_animation != "swing_sword_1":
+			anim_player.play("swing_sword_1")
+	elif running:
+		if anim_player.current_animation != "run":
+			anim_player.play("run")
+	else:
+		if anim_player.current_animation != "idle" and is_on_floor():
+			anim_player.play("idle")
+
+func set_sword_hitbox(value: bool) -> void:
+	swinging = value
+	hitbox.set_collision_layer_value(1, swinging)
