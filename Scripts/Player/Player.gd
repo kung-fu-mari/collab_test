211f1a7c2c500816ec009aa4573122e53e83e43f
kung-fu-mari <ifcodes@duck.com>
2025-03-05 19:10:16 -0700

    Add double jump, states var

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -5,21 +5,30 @@ extends CharacterBody2D
 
 const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
+const MAX_JUMPS := 2
 
-var running := false
-var swinging := false
+var jumps_left := MAX_JUMPS
+var current_state : String = "idle"
+#var running := false
+#var swinging := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if is_on_floor():
+		jumps_left = MAX_JUMPS
+		
+	if Input.is_action_just_pressed("up") and jumps_left > 0:
+		jumps_left -= 1
 		velocity.y = JUMP_VELOCITY
+		
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		running = true
+		if current_state != "swinging":
+			current_state = "running"
 		if direction < 0:
 			$SpriteContainer.scale.x = -1
 		else:
@@ -27,7 +36,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		if velocity.x == 0:
-			running = false
+			if current_state != "swinging":
+				current_state = "idle"
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 	
@@ -38,15 +48,15 @@ func _process(_delta: float) -> void:
 	handle_animation()
 
 func check_for_attack() -> void:
-	if Input.is_action_pressed("attack") and swinging == false:
-		swinging = true
+	if Input.is_action_pressed("attack") and current_state != "swinging":
+		current_state = "swinging"
 		hitbox.set_collision_layer_value(1, true)
 
 func handle_animation() -> void:
-	if swinging:
+	if current_state == "swinging":
 		if anim_player.current_animation != "swing_sword_1":
 			anim_player.play("swing_sword_1")
-	elif running:
+	elif current_state == "running":
 		if anim_player.current_animation != "run":
 			anim_player.play("run")
 	else:
@@ -54,5 +64,8 @@ func handle_animation() -> void:
 			anim_player.play("idle")
 
 func set_sword_hitbox(value: bool) -> void:
-	swinging = value
-	hitbox.set_collision_layer_value(1, swinging)
+	if value:
+		current_state = "swinging"
+	else:
+		current_state = ""
+	hitbox.set_collision_layer_value(1, value)
